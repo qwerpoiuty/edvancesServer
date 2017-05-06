de7b1ce9b6f8bea7974e94d2871a2e633d706f38
Stan Le <stan.le1107@gmail.com>
2017-05-06 11:24:15 -0700

    more messages

--- browser/js/unauthorized/teacherDetail/teacherDetail.js
+++ browser/js/unauthorized/teacherDetail/teacherDetail.js
@@ -11,7 +11,7 @@ app.config(function($stateProvider) {
                 })
             },
             classes: function(classroomFactory, $stateParams) {
-                return classroomFactory.getClassroomsByUser($stateParams.id).then(classrooms => {
+                return classroomFactory.getClassroomsByTeacher($stateParams.id).then(classrooms => {
                     return classrooms
                 })
             }
