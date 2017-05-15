01cd430b6b46718c56a5adb5b3cadcd84d24c28a
Stan Le <stan.le1107@gmail.com>
2017-05-15 01:35:36 -0700

    environment

--- server/env/index.js
+++ server/env/index.js
@@ -1,5 +1,5 @@
-// var path = require('path');
-// var devConfigPath = path.join(__dirname, './development.js');
+var path = require('path');
+var devConfigPath = path.join(__dirname, './development.js');
 // var productionConfigPath = path.join(__dirname, './production.js');
 // var testConfigPath = path.join(__dirname, './testing.js');
 
