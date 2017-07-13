0b56c580afbf8fe129ae6ddf9131a547d3fd6932
Stan Le <stan.le1107@gmail.com>
2017-07-13 16:23:19 -0700

    more azure tests

--- server/env/index.js
+++ server/env/index.js
@@ -4,7 +4,7 @@ var productionConfigPath = path.join(__dirname, './production.js');
 var testConfigPath = path.join(__dirname, './testing.js');
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require(productionConfigPath);
+    module.exports = require(devConfigPath);
 } else if (process.env.NODE_ENV === 'testing') {
     module.exports = require(testConfigPath);
 } else {
