03fca94bc77afd56f043a51a73eabd841313eef2
Stan Le <stan.le1107@gmail.com>
2017-07-13 18:04:10 -0700

    hmmm...

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,13 +2,13 @@
 var router = require('express').Router();
 module.exports = router;
 
-// router.use('/users', require('./users'));
-// router.use('/classrooms', require('./classrooms'));
-// router.use('/documents', require('./documents'));
-// router.use('/lessons', require('./lessons'));
-// router.use('/quizzes', require('./quizzes'));
-// router.use('/transactions', require('./transactions'))
-// router.use('/forums', require('./forums'))
+router.use('/users', require('./users'));
+router.use('/classrooms', require('./classrooms'));
+router.use('/documents', require('./documents'));
+router.use('/lessons', require('./lessons'));
+router.use('/quizzes', require('./quizzes'));
+router.use('/transactions', require('./transactions'))
+router.use('/forums', require('./forums'))
 // Make sure this is after all of
 // the registered routes!
 router.use(function(req, res) {
